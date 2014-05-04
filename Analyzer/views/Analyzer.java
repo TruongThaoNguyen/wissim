01cd6b8e7dd308b7de6157793057181d0c54181b
kakalili <linhtrinhkhac@gmail.com>
2014-05-04 21:10:45 +0700

    --fix analyzer--

--- Analyzer/views/Analyzer.java
+++ Analyzer/views/Analyzer.java
@@ -1,8 +1,28 @@
 package views;
 
+import java.io.IOException;
+import java.util.MissingResourceException;
+
+import main.DelayTab;
+import main.EfficiencyTab;
+import main.EnergyTab;
+import main.HopCountTab;
+import main.NetworkLifeTimeTab;
+import main.SleepPeriodTab;
+import main.Tab;
+import main.ThroughputTab;
+
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.StatusLineManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+
+import parser.TraceFile;
+
+import com.ibm.icu.text.MessageFormat;
 
 import views.MainContent;
 
@@ -13,8 +33,15 @@ public class Analyzer extends MainContent {
 	 * @param parent
 	 * @param style
 	 */
+	public TabFolder tabFolder;
 	public Analyzer(Composite parent, MenuManager menuManager, StatusLineManager statusLineManager) {
 		super(parent, menuManager, statusLineManager);
+		try {
+			TraceFile.ConvertTraceFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		createContents();
 		
 	}
 
@@ -22,5 +49,53 @@ public class Analyzer extends MainContent {
 	protected void updateMenu() {
 				
 	}
+	
+	public void createContents() {
+		setLayout(new GridLayout(1, false));
+		tabFolder = new TabFolder(this, SWT.NULL);
+	    Tab[] tabs = new Tab[] { new ThroughputTab(this),new DelayTab(this),new HopCountTab(this),
+	    						new EfficiencyTab(this),new EnergyTab(this),new NetworkLifeTimeTab(this),new SleepPeriodTab(this)};
+	    for (int i = 0; i < tabs.length; i++) {
+	      TabItem item = new TabItem(tabFolder, SWT.NULL);
+	      item.setText(tabs[i].getTabText());
+	      item.setControl(tabs[i].createTabFolderPage(tabFolder));
+	    }
+	}
+	
+	/**
+	   * Gets a string from the resource bundle. We don't want to crash because of
+	   * a missing String. Returns the key if not found.
+	   */
+	  public static String getResourceString(String key) {
+	      return key;
+	  }
 
+	  /**
+	   * Gets a string from the resource bundle and binds it with the given
+	   * arguments. If the key is not found, return the key.
+	   */
+	  public static String getResourceString(String key, Object[] args) {
+	    try {
+	      return MessageFormat.format(getResourceString(key), args);
+	    } catch (MissingResourceException e) {
+	      return key;
+	    } catch (NullPointerException e) {
+	      return "!" + key + "!";
+	    }
+	  }
+
+	  /**
+	   * Grabs input focus.
+	   */
+//	  public void setFocus() {
+//	    tabFolder.setFocus();
+//	  }
+
+	  /**
+	   * Disposes of all resources associated with a particular instance of the
+	   * LayoutExample.
+	   */
+	  public void dispose() {
+	    tabFolder = null;
+	  }
 }
