f5a62995ef335db71f2c240ec6aa4bd711e19182
Trong Nguyen <ductrongcp.hut@gmail.com>
2014-07-15 16:15:57 +0700

    update java doc

--- Editor/controllers/synchronizer/Scheduler.java
+++ Editor/controllers/synchronizer/Scheduler.java
@@ -2,8 +2,28 @@ package controllers.synchronizer;
 
 import java.util.HashMap;
 
+/**
+ * Interface for schedulable objects.
+ * @author trongnguyen
+ */
 public interface Scheduler {	
+	/**
+	 * add an event to scheduler of this object.
+	 * @param time time of event
+	 * @param arg argument of event
+	 */
 	void 	addEvent(double time, String arg);
+	
+	/**
+	 * get time of an event from scheduler.
+	 * @param arg argument of event
+	 * @return time of this event. return null if arg isn't match with any event
+	 */
 	double	getEvent(String arg);
+	
+	/**
+	 * get all events.
+	 * @return Hashmap include all event
+	 */
 	HashMap<String, Double> getEvent();
 }
--- Editor/controllers/synchronizer/TclObject.java
+++ Editor/controllers/synchronizer/TclObject.java
@@ -8,35 +8,94 @@ import models.converter.InsProc;
 import models.converter.InsVar;
 
 /**
- * TclObject.java
- * @Copyright (C) 2014, Sedic Laboratory, Hanoi University of Science and Technology
+ * Interface for Tcl shadow objects.
+ * Tcl shadow objects is "shadow" of fable tcl object.
+ * this interface include general method of all shadow object.
  * @Author Duc-Trong Nguyen
- * @Version 2.0
  */
-public interface TclObject 
-{	
-	/**
-	 * Parse by call InstProc
-	 * @param command Token list, first token is identify of insProc that will used
-	 * @return TclObject, result of InsProc
-	 * @throws Exception
-	 */
-	//String parse(List<String> command) throws Exception;
+public interface TclObject {
 	
+	/**
+	 * Parse a tcl command and call corresponding InstProc. 
+	 * @param command Token list, first token is identification of insProc that will used
+	 * @param isRecord this command will be recorded or not
+	 * @return String, result of InsProc
+	 * @throws Exception Parse exception
+	 */	
 	String parse(List<String> command, boolean isRecord) throws Exception;
 	
+	/**
+	 * get label of this object.
+	 * @return string is label of object
+	 */
 	String	getLabel();	
+	
+	/**
+	 * set label of this object.
+	 * @param label new label to this object
+	 */
 	void	setLabel(String label);
 	
+	/**
+	 * add an InsProc to this object.
+	 * Each InsProc is corresponding of a tcl command.  
+	 * @param p new insproc
+	 */
 	void 	addInsProc(InsProc p);
+	
+	/**
+	 * get an insproc with key. Key is name of command that corresponding with InsProc.
+	 * @param key key of Insproc.
+	 * @return InsProc
+	 */
 	InsProc getInsProc(String key);	
 		
+	/**
+	 * set an InsVar. Label of InsVar is its value.
+	 * @param key key of InsVar. (name of variable)
+	 * @param value value of InsVar. (value of variable)
+	 * @return an InsVar.
+	 */
 	InsVar 	setInsVar(String key, String value);
+	
+	/**
+	 * set an InsVar.
+	 * @param key key of InsVar. (name of variable)
+	 * @param value value of InsVar. (value of variable)
+	 * @param label label of InsVar.
+	 * @return InsVar
+	 */
 	InsVar	setInsVar(String key, String value, String label);
-	InsVar 	getInsVar(String key);	
+	
+	/**
+	 * get an InsVar.
+	 * @param key key of InsVar. (name of variable)
+	 * @return InsVar
+	 */
+	InsVar 	getInsVar(String key);
+	
+	/**
+	 * get all InsVar.
+	 * @return HashMap that included all InsVar.
+	 */
 	HashMap<String, InsVar> getInsVar();
 	
-	void 		addEntry(Entry e);
-	void		addEntry(int index, Entry e);
+	/**
+	 * add an Entry to register list.
+	 * @param e new entry
+	 */
+	void addEntry(Entry e);
+	
+	/**
+	 * add an Entry to register list.
+	 * @param index index in list to be add.
+	 * @param e new entry.
+	 */
+	void addEntry(int index, Entry e);
+	
+	/**
+	 * get all entries.
+	 * @return list of entries.
+	 */
 	List<Entry> getEntry();
 }
